9ba2d64070e161cb84242445315252fb2f7092fe
S.M. Al Mamun <sharif.mamun@gmail.com>
2013-03-26 16:53:00 -0500

    Create wordle.py

---
+++ wordle.py
@@ -0,0 +1,192 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Last updated on: March 22-23, 2013
+import sys
+import os
+from collections import defaultdict
+from Tkinter import Tk, Canvas, Frame, BOTH, W
+import tkMessageBox
+from random import choice
+import random
+
+# All supported colors in Tkinter
+COLORS  =['snow', 'ghost white', 'white smoke', 'gainsboro', 'floral white', 'old lace',
+    'linen', 'antique white', 'papaya whip', 'blanched almond', 'bisque', 'peach puff',
+    'navajo white', 'lemon chiffon', 'mint cream', 'azure', 'alice blue', 'lavender',
+    'lavender blush', 'misty rose', 'dark slate gray', 'dim gray', 'slate gray',
+    'light slate gray', 'gray', 'light grey', 'midnight blue', 'navy', 'cornflower blue', 'dark slate blue',
+    'slate blue', 'medium slate blue', 'light slate blue', 'medium blue', 'royal blue',  'blue',
+    'dodger blue', 'deep sky blue', 'sky blue', 'light sky blue', 'steel blue', 'light steel blue',
+    'light blue', 'powder blue', 'pale turquoise', 'dark turquoise', 'medium turquoise', 'turquoise',
+    'cyan', 'light cyan', 'cadet blue', 'medium aquamarine', 'aquamarine', 'dark green', 'dark olive green',
+    'dark sea green', 'sea green', 'medium sea green', 'light sea green', 'pale green', 'spring green',
+    'lawn green', 'medium spring green', 'green yellow', 'lime green', 'yellow green',
+    'forest green', 'olive drab', 'dark khaki', 'khaki', 'pale goldenrod', 'light goldenrod yellow',
+    'light yellow', 'yellow', 'gold', 'light goldenrod', 'goldenrod', 'dark goldenrod', 'rosy brown',
+    'indian red', 'saddle brown', 'sandy brown',
+    'dark salmon', 'salmon', 'light salmon', 'orange', 'dark orange',
+    'coral', 'light coral', 'tomato', 'orange red', 'red', 'hot pink', 'deep pink', 'pink', 'light pink',
+    'pale violet red', 'maroon', 'medium violet red', 'violet red',
+    'medium orchid', 'dark orchid', 'dark violet', 'blue violet', 'purple', 'medium purple',
+    'thistle', 'snow2', 'snow3',
+    'snow4', 'seashell2', 'seashell3', 'seashell4', 'AntiqueWhite1', 'AntiqueWhite2',
+    'AntiqueWhite3', 'AntiqueWhite4', 'bisque2', 'bisque3', 'bisque4', 'PeachPuff2',
+    'PeachPuff3', 'PeachPuff4', 'NavajoWhite2', 'NavajoWhite3', 'NavajoWhite4',
+    'LemonChiffon2', 'LemonChiffon3', 'LemonChiffon4', 'cornsilk2', 'cornsilk3',
+    'cornsilk4', 'ivory2', 'ivory3', 'ivory4', 'honeydew2', 'honeydew3', 'honeydew4',
+    'LavenderBlush2', 'LavenderBlush3', 'LavenderBlush4', 'MistyRose2', 'MistyRose3',
+    'MistyRose4', 'azure2', 'azure3', 'azure4', 'SlateBlue1', 'SlateBlue2', 'SlateBlue3',
+    'SlateBlue4', 'RoyalBlue1', 'RoyalBlue2', 'RoyalBlue3', 'RoyalBlue4', 'blue2', 'blue4',
+    'DodgerBlue2', 'DodgerBlue3', 'DodgerBlue4', 'SteelBlue1', 'SteelBlue2',
+    'SteelBlue3', 'SteelBlue4', 'DeepSkyBlue2', 'DeepSkyBlue3', 'DeepSkyBlue4',
+    'SkyBlue1', 'SkyBlue2', 'SkyBlue3', 'SkyBlue4', 'LightSkyBlue1', 'LightSkyBlue2',
+    'LightSkyBlue3', 'LightSkyBlue4', 'SlateGray1', 'SlateGray2', 'SlateGray3',
+    'SlateGray4', 'LightSteelBlue1', 'LightSteelBlue2', 'LightSteelBlue3',
+    'LightSteelBlue4', 'LightBlue1', 'LightBlue2', 'LightBlue3', 'LightBlue4',
+    'LightCyan2', 'LightCyan3', 'LightCyan4', 'PaleTurquoise1', 'PaleTurquoise2',
+    'PaleTurquoise3', 'PaleTurquoise4', 'CadetBlue1', 'CadetBlue2', 'CadetBlue3',
+    'CadetBlue4', 'turquoise1', 'turquoise2', 'turquoise3', 'turquoise4', 'cyan2', 'cyan3',
+    'cyan4', 'DarkSlateGray1', 'DarkSlateGray2', 'DarkSlateGray3', 'DarkSlateGray4',
+    'aquamarine2', 'aquamarine4', 'DarkSeaGreen1', 'DarkSeaGreen2', 'DarkSeaGreen3',
+    'DarkSeaGreen4', 'SeaGreen1', 'SeaGreen2', 'SeaGreen3', 'PaleGreen1', 'PaleGreen2',
+    'PaleGreen3', 'PaleGreen4', 'SpringGreen2', 'SpringGreen3', 'SpringGreen4',
+    'green2', 'green3', 'green4', 'chartreuse2', 'chartreuse3', 'chartreuse4',
+    'OliveDrab1', 'OliveDrab2', 'OliveDrab4', 'DarkOliveGreen1', 'DarkOliveGreen2',
+    'DarkOliveGreen3', 'DarkOliveGreen4', 'khaki1', 'khaki2', 'khaki3', 'khaki4',
+    'LightGoldenrod1', 'LightGoldenrod2', 'LightGoldenrod3', 'LightGoldenrod4',
+    'LightYellow2', 'LightYellow3', 'LightYellow4', 'yellow2', 'yellow3', 'yellow4',
+    'gold2', 'gold3', 'gold4', 'goldenrod1', 'goldenrod2', 'goldenrod3', 'goldenrod4',
+    'DarkGoldenrod1', 'DarkGoldenrod2', 'DarkGoldenrod3', 'DarkGoldenrod4',
+    'RosyBrown1', 'RosyBrown2', 'RosyBrown3', 'RosyBrown4', 'IndianRed1', 'IndianRed2',
+    'IndianRed3', 'IndianRed4', 'sienna1', 'sienna2', 'sienna3', 'sienna4', 'burlywood1',
+    'burlywood2', 'burlywood3', 'burlywood4', 'wheat1', 'wheat2', 'wheat3', 'wheat4', 'tan1',
+    'tan2', 'tan4', 'chocolate1', 'chocolate2', 'chocolate3', 'firebrick1', 'firebrick2',
+    'firebrick3', 'firebrick4', 'brown1', 'brown2', 'brown3', 'brown4', 'salmon1', 'salmon2',
+    'salmon3', 'salmon4', 'LightSalmon2', 'LightSalmon3', 'LightSalmon4', 'orange2',
+    'orange3', 'orange4', 'DarkOrange1', 'DarkOrange2', 'DarkOrange3', 'DarkOrange4',
+    'coral1', 'coral2', 'coral3', 'coral4', 'tomato2', 'tomato3', 'tomato4', 'OrangeRed2',
+    'OrangeRed3', 'OrangeRed4', 'red2', 'red3', 'red4', 'DeepPink2', 'DeepPink3', 'DeepPink4',
+    'HotPink1', 'HotPink2', 'HotPink3', 'HotPink4', 'pink1', 'pink2', 'pink3', 'pink4',
+    'LightPink1', 'LightPink2', 'LightPink3', 'LightPink4', 'PaleVioletRed1',
+    'PaleVioletRed2', 'PaleVioletRed3', 'PaleVioletRed4', 'maroon1', 'maroon2',
+    'maroon3', 'maroon4', 'VioletRed1', 'VioletRed2', 'VioletRed3', 'VioletRed4',
+    'magenta2', 'magenta3', 'magenta4', 'orchid1', 'orchid2', 'orchid3', 'orchid4', 'plum1',
+    'plum2', 'plum3', 'plum4', 'MediumOrchid1', 'MediumOrchid2', 'MediumOrchid3',
+    'MediumOrchid4', 'DarkOrchid1', 'DarkOrchid2', 'DarkOrchid3', 'DarkOrchid4',
+    'purple1', 'purple2', 'purple3', 'purple4', 'MediumPurple1', 'MediumPurple2',
+    'MediumPurple3', 'MediumPurple4', 'thistle1', 'thistle2', 'thistle3', 'thistle4',
+    'gray1', 'gray2', 'gray3', 'gray4', 'gray5', 'gray6', 'gray7', 'gray8', 'gray9', 'gray10',
+    'gray11', 'gray12', 'gray13', 'gray14', 'gray15', 'gray16', 'gray17', 'gray18', 'gray19',
+    'gray20', 'gray21', 'gray22', 'gray23', 'gray24', 'gray25', 'gray26', 'gray27', 'gray28',
+    'gray29', 'gray30', 'gray31', 'gray32', 'gray33', 'gray34', 'gray35', 'gray36', 'gray37',
+    'gray38', 'gray39', 'gray40', 'gray42', 'gray43', 'gray44', 'gray45', 'gray46', 'gray47',
+    'gray48', 'gray49', 'gray50', 'gray51', 'gray52', 'gray53', 'gray54', 'gray55', 'gray56',
+    'gray57', 'gray58', 'gray59', 'gray60', 'gray61', 'gray62', 'gray63', 'gray64', 'gray65',
+    'gray66', 'gray67', 'gray68', 'gray69', 'gray70', 'gray71', 'gray72', 'gray73', 'gray74',
+    'gray75', 'gray76', 'gray77', 'gray78', 'gray79', 'gray80', 'gray81', 'gray82', 'gray83',
+    'gray84', 'gray85', 'gray86', 'gray87', 'gray88', 'gray89', 'gray90', 'gray91', 'gray92',
+    'gray93', 'gray94', 'gray95', 'gray97', 'gray98', 'gray99']
+
+class guiCreator(Frame):  
+    def __init__(self, parent,arg):
+        Frame.__init__(self, parent)   
+        self.arg = arg
+        self.parent = parent        
+        self.initUI()
+        
+    def initUI(self):
+        # Setting title as wordle      
+        self.parent.title("Wordle")        
+        self.pack(fill=BOTH, expand=1)
+        #print "Inside Canvas", self.arg
+        # Task 5 - Pad outside border by 50 pixels all around
+        # Canvas with 50 pixels outside border
+        canvas = Canvas(self, bd=50, bg = "white")
+        # Two helper variables
+        # for generating start position of a word
+
+        # Task 6 - Random color for each word
+        # Generating Random colors for each of the word
+        for word, value in self.arg.items():
+            # Task 7 - Font size based on frequency of the words
+            # Generating font size based on the frequency of the words!
+            # Very simple logic
+            # Could make it more complex but didn't do that!
+            if value > 15:
+                i = random.randrange(150,400, 70)
+                j = random.randrange(150,600, 50)
+            else:    
+                i = random.randrange(200,600, 27)
+                j = random.randrange(100,600, 33)
+
+            canvas.create_text(i, j, anchor=W, font=("times",(value*2)),
+                text=word, fill=choice(COLORS))
+        canvas.pack(fill=BOTH, expand=1)
+
+
+def main():
+    """ This program will create Wordle like output based on the following conditions:
+    1 - read a text, like the "Declaration of Independence" from a file
+    2 - Eliminate the common words - using a common word file
+    3 - Find the frequency of the uncommon words
+    4 - Eliminate the words that occur less than 4 times
+    5 - Pad outside border by 50 pixels all around
+    6 - Random color for each word
+    7 - Font size based on frequency of the words
+    8 - Draw all the words using the specified font sizes
+    9 - When user presses spacebar, words are re-drawn
+    10 - Pick new locations and new colors for each update.
+    """
+
+    # Task 1
+    # Reading text file from "sample.txt"
+    user_input = raw_input("Enter the name of the input file: \n")
+    if os.path.exists(user_input):        
+        fin = open(user_input, "rU")
+    else:
+        print "File not found."
+        os._exit(1)
+
+    wordList = []
+    for line in fin:
+        for word in line.strip().split(" "):
+            #print word
+            if word.endswith("?") or word.endswith(".") or word.endswith("!") or word.endswith(","):
+                wordList.append(word[0:len(word)-1].upper())
+            else:
+                wordList.append(word.upper())
+    #print wordList
+    wordList_set = set(wordList)
+    #print wordList_set
+    #print "Total words = %d" % wordCount
+    # Reading the common word file
+    # Task 2, Part 1: reading the common words
+    # 
+    delete_set = set(line.strip().upper() for line in open('common.txt'))
+    #print delete_set
+
+    # Task 2, Part 2: eleminating the common words
+    setOfWords = (wordList_set - delete_set)
+    list_setOfWords = list(setOfWords)
+    #print list(setOfWords)
+    #print list_setOfWords
+    
+    # Task 3: Find the frequency of the uncommon words
+    result_dict = dict( [ (i, wordList.count(i)) for i in set(list_setOfWords) ] )
+    #print result_dict
+
+    # Task 4 - Eliminate the words that occur less than 4 times
+    for name, age in result_dict.items():
+        if age < 4:
+            del result_dict[name]
+    #print result_dict
+
+    # Calling Tkinter Frame
+    # to draw the canvas with the words
+    root = Tk()
+    ex = guiCreator(root, result_dict)
+    root.geometry("800x800+300+300")
+    root.mainloop()  
+    
+if  __name__ =='__main__':
+    main()
